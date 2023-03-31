eb7f1689cd01a177e5ff52f7e882ffccaf51bb73
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2023-03-31 22:25:50 +0300

    Update ColorController.cs

--- Assets/Scripts/Buttons/Colors/ColorController.cs
+++ Assets/Scripts/Buttons/Colors/ColorController.cs
@@ -4,17 +4,16 @@ namespace Buttons.Colors
 {
     public class ColorController : MonoBehaviour
     {   
-        public GameObject target;
-
-        public int targetBodyChildObjects = 3;
+        public GameObject target;        
     
         public void SetColor(string hexColorCode)
         {
             ColorUtility.TryParseHtmlString(hexColorCode, out var color);
         
-            for (var countChildObjects = 0; countChildObjects < targetBodyChildObjects; countChildObjects++)
+            for (var countChildObjects = 0; countChildObjects < target.transform.childCount; countChildObjects++)
             {
                 target.transform.GetChild(countChildObjects).GetComponent<MeshRenderer>().material.color = color;
+                print(transform.childCount);
             }
         }   
     }
