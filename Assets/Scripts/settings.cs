5bad3f79ac07692f6de4d437ba47bb4b1034d0cf
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2023-04-28 12:09:26 +0300

    menu settings

--- Assets/Scripts/settings.cs
+++ Assets/Scripts/settings.cs
@@ -1,24 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class settings : MonoBehaviour
 {
     public CameraController _cameraController;    
     public GameObject PanelSettings;
 
+    public List<Slider> sliders = new List<Slider>();
+    public List<Text> perTexts = new List<Text>();
+
     void Start()
     {
-        // _cameraController = GetComponent<CameraController>();
+
     }
     
     void Update()
     {
-        
+        SettingsValues();
     }
 
     void SettingsValues()
     {
-
+        for (int count = 0; count < perTexts.Count; count++)
+        {
+            perTexts[count].text = sliders[count].value.ToString() + "%";
+        }
+        // TextPerSensivityComp.text = SliderSensivityComp.value.ToString() + "%";
+        // TextPerZoomSpeedComp.text = SliderZoomSpeedComp.value.ToString() + "%";
+        // TextPerDesktopDeltaComp.text = SliderDesktopDeltaComp.value.ToString() + "%";
+        // TextPerDownLimitComp.text = SliderDownLimitComp.value.ToString() + "%";
+        // TextPerUpLimitComp.text = SliderUpLimitComp.value.ToString() + "%";
+        // TextPerMinDistanceComp.text = SliderMinDistanceComp.value.ToString() + "%";
+        // TextPerMaxDistanceComp.text = SliderMaxDistanceComp.value.ToString() + "%";
     }
 }
