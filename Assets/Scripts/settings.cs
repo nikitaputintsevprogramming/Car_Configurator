f26f11cf8ed9a387e5b57bd26edc18d2f1333f36
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2023-04-28 10:19:15 +0300

    settings menu

---
+++ Assets/Scripts/settings.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class settings : MonoBehaviour
+{
+    public CameraController _cameraController;    
+    public GameObject PanelSettings;
+
+    void Start()
+    {
+        // _cameraController = GetComponent<CameraController>();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    void SettingsValues()
+    {
+
+    }
+}
