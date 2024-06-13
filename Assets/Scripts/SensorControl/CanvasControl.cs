4224a5575dc03110db19d3ba96128a45d2760549
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2024-06-13 10:40:57 +0300

    canvasControl

---
+++ Assets/Scripts/SensorControl/CanvasControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class CanvasControl : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData data)
+    {
+
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+
+    }
+
+    public void OnEndDrag(PointerEventData data)
+    {
+
+    }
+}
