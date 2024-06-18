58a4d85b1f0cb90051081783e5cdbf57bf8b1b04
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2024-06-18 09:44:24 +0300

    log scriptable obj popup

---
+++ Assets/Scripts/PopUps/Popup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.PopUps
+{
+    public class Popup : MonoBehaviour
+    {
+        [SerializeField] private PopupData popUpData;
+
+        private void OnMouseDown()
+        {
+            Debug.Log(popUpData.name);
+            Debug.Log(popUpData.PopupName);
+            Debug.Log(popUpData.PopupDescription);
+            //Debug.Log(popUpData.PopupIcon.name);
+        }
+    }
+}
---
+++ Assets/Scripts/PopUps/PopupData.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.PopUps
+{
+    [CreateAssetMenu(fileName = "new PopupData", menuName = "PopupData", order = 51)]
+    public class PopupData : ScriptableObject
+    {
+        [SerializeField] private string popupName;
+        [SerializeField] private string popupDescription;
+        [SerializeField] private Sprite popupIcon;
+
+        public string PopupName
+        {
+            get
+            {
+                return popupName;
+            }
+        }
+
+        public string PopupDescription
+        {
+            get
+            {
+                return popupDescription;
+            }
+        }
+
+        public Sprite PopupIcon
+        {
+            get
+            {
+                return popupIcon;
+            }
+        }
+    }
+}
