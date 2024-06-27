7f314bce31e252a3eaf9e802847f2bafebe156c6
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2024-06-27 11:12:39 +0300

    точка невозврата

--- Assets/Scripts/Labels/FlagController.cs
+++ Assets/Scripts/Labels/FlagController.cs
@@ -12,38 +12,35 @@ namespace T34
         //public Vector3 flagPosition; // Позиция флага
         public GameObject labelCanvas; // Панель для отображения названия и описания 
         public TMP_Text titleText; // Текст для названия
-        public TMP_Text descriptionText; // Текст для описания
         //public LineRenderer lineRenderer; // Линия для указания на панель
 
         void Start()
         {
             labelCanvas.SetActive(false);
             titleText.gameObject.SetActive(false);
-            descriptionText.gameObject.SetActive(false);
         }
         private void OnEnable()
         {
             LabelLookAt labelLookAt = FindObjectOfType<LabelLookAt>();
             labelLookAt.OnActionTouchLabel += ActionTouchLabel;
+            
+            CanvasControl canvasControl = FindObjectOfType<CanvasControl>();
+            canvasControl.OnActionOnDrag += ActionEvents_BeginDrag;
+        }
+
+        private void ActionEvents_BeginDrag(object sender, EventArgs e)
+        {
+            labelCanvas.SetActive(false);
+            titleText.gameObject.SetActive(false);
         }
 
         private void ActionTouchLabel(object sender, EventArgs e)
         {
             labelCanvas.SetActive(true);
             titleText.gameObject.SetActive(true);
-            descriptionText.gameObject.SetActive(true);
             //Vector3 targetPos = labelCanvas.GetComponent<RectTransform>() - Camera.main.transform.position;
             labelCanvas.GetComponent<RectTransform>().LookAt(Camera.main.transform);
         }
-
-        //private void Update()
-        //{
-        //    //if(Input.GetKey(KeyCode.W))
-        //    //{
-        //        //Vector3 targetPos = uiPanel.transform.position - Camera.main.transform.position;
-        //        labelCanvas.GetComponent<RectTransform>().LookAt(Camera.main.transform);
-        //    //}
-        //}
     }
 }
 
